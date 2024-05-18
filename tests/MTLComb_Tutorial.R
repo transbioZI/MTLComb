83bf5955f111d3593273d18e4fa049124491307d
Han.Cao <hank9cao@gmail.com>
2024-05-18 16:01:25 +0200

    ss

--- tests/MTLComb_Tutorial.R
+++ tests/MTLComb_Tutorial.R
@@ -24,14 +24,46 @@ library("MTLComb")
 
 # Create 20 tasks, First 10 are regression tasks, and the second 10 is classification tasks.
 # Each task contains 200 features, 100 subjects. The first 10 features are true predictive features
-source ("tests/generate_simulation_data.R")
+
+#generate classification tasks
+t <- 10 # number of classification tasks
+p <- 200 # number of features
+n <- 100 # number of subjects
+W <- matrix(data=rnorm(t*p),ncol=t, nrow = p) # create coefficient matrix
+W[1:p*0.9,] <- 0 # create sparsity
+X <- list(); Y <- list(); tX <- list(); tY <- list()
+for(i in 1:t){
+  X[[i]] <- matrix(data=rnorm(n*p),ncol=p, nrow = n) # feature matrix
+  Y[[i]] <- sign(X[[i]] %*% W[,i] + 0.5 * rnorm(n)) # outcome
+  tX[[i]] <- matrix(rnorm(p*n),nrow=n) #test data
+  tY[[i]] <- sign(tX[[i]] %*% W[, i] + rnorm(n) * 0.5) #test data
+}
+cX <- X; cY <- Y; cW <- W; ctX <- tX; ctY <- tY;
+
+#generate regression tasks
+t <- 10 # number of regression tasks
+p <- 200 # number of features
+n <- 100 # number of subjects
+W <- matrix(data=rnorm(t*p),ncol=t, nrow = p) # create coefficient matrix
+W[1:p*0.9,] <- 0 # create sparsity
+X <- list(); Y <- list(); tX <- list(); tY <- list()
+for(i in 1:t){
+  X[[i]] <- matrix(data=rnorm(n*p),ncol=p, nrow = n) # feature matrix
+  Y[[i]] <- X[[i]] %*% W[,i] + 0.5 * rnorm(n) # outcome
+  tX[[i]] <- matrix(rnorm(p*n),nrow=n) #test data
+  tY[[i]] <- tX[[i]] %*% W[, i] + rnorm(n) * 0.5 #test data
+}
+
+X <- c(X,cX); Y <- c(Y,cY); W <- cbind(W,cW);
+tX <- c(tX, ctX); tY <- c(tY, ctY)
 
 
 
 
 #parameters
 opts <- list(init=0, tol=10^-6, maxIter=10000, ter=2)
-ctasks
+ctasks <- 11:20
+
 
 
 
